3bbf26e95b1d4dd8bbbaf69d9bb5766454088151
armariya <arm.armariya@gmail.com>
2018-04-12 15:37:40 +0700

    delete SimpleStorage from 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,5 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
 const SlotMachineSpinner = artifacts.require("./SlotMachineSpinner.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
   deployer.deploy(SlotMachineSpinner);
 };
